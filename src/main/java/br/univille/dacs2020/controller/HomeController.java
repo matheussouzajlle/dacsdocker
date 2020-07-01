7f2506b7f1af71ad8a58627bab0608e1031e5c88
MATHEUS DE SOUZA <matheussouza.1@univille.br>
2020-07-01 22:46:09 +0000

    primeira versao

---
+++ src/main/java/br/univille/dacs2020/controller/HomeController.java
@@ -0,0 +1,23 @@
+package br.univille.dacs2020.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/")
+@Controller
+public class HomeController {
+   
+    @GetMapping("")
+    public ModelAndView index(){
+        Date agora = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY - HH:mm");
+        String data = sdf.format(agora);
+
+        return new ModelAndView("home/index","dataatualizada",data);
+    }
+}
